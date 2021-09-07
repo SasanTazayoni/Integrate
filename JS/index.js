22196f21a175c92b7eed4c84a2baba39e0d28cdd
Sasan <sasantazayoni@gmail.com>
2021-09-07 15:31:47 +0100

    Collapsible component with JS.

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,16 @@
-function menu() {
-    var list = document.getElementsByClassName('list')[0]
-    var display = list.style.display
-    if (!display) {
-        list.style.display = 'block'
-    } else {
-        list.style.display = ''
-    }
-    
-}
+// function menu() {
+//     var list = document.getElementsByClassName('list')[0]
+//     var display = list.style.display
+//     if (!display) {
+//         list.style.display = 'block'
+//     } else {
+//         list.style.display = ''
+//     } 
+// }
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
