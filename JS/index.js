71300b278d6b145288b608dc2d6092b4db67298d
Sasan <sasantazayoni@gmail.com>
2021-09-17 17:50:40 +0100

    Navigation bar complete.

--- JS/index.js
+++ JS/index.js
@@ -9,8 +9,41 @@
 // }
 
 const collapsibles = document.querySelectorAll(".collapsible");
+
 collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
+  item.querySelector('.collapsible__icon .icon').addEventListener("click", function (event) {
+    console.log({event});
+    item.classList.toggle("collapsible--expanded");
   })
 );
+
+/*
+// Fire only if target is nav...
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function (event) {
+    console.log({event});
+    if (event.target === item) {
+      item.classList.toggle("collapsible--expanded");
+    }
+  })
+);*/
+
+/*
+// Stop propagation of click event
+document.querySelectorAll('.nav__item').forEach(li => {
+  li.addEventListener('click', (event) => {
+    event.stopPropagation();
+  })
+})*/
+
+// const dropDown = () => {
+//   const collapsible__icon = document.querySelector('.collapsible__icon');
+//   const collapsible__content = document.querySelector('.collapsible__content');
+
+//   collapsible__icon.addEventListener('click', () => {
+//     collapsible__content.classList.toggle("collapsible--expanded");
+
+//   });
+// }
+
+// dropDown();
