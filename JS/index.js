768086e020f6ed5f06969656c02cacadafed02f0
Sasan <sasantazayoni@gmail.com>
2021-11-01 16:07:56 +0000

    Bug fix for small screens on questions block and background image optimisation.

--- JS/index.js
+++ JS/index.js
@@ -7,6 +7,35 @@ collapsibles.forEach((item) =>
   })
 );
 
+const CheckWebp = {
+  init() {
+    this.check_webp_feature('alpha', (f, supported) => {
+      if (supported) document.body.classList.add('webp');
+    })
+  },
+
+  check_webp_feature(feature, callback) {
+    // 
+    var kTestImages = {
+        lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+        lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+        alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+        animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
+    };
+    var img = new Image();
+    img.onload = function () {
+        var result = (img.width > 0) && (img.height > 0);
+        callback(feature, result);
+    };
+    img.onerror = function () {
+        callback(feature, false);
+    };
+    img.src = "data:image/webp;base64," + kTestImages[feature];
+  }
+};
+
+CheckWebp.init();
+
 const Testimonials = {
   config: {
     // Time in seconds
